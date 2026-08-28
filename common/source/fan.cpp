ea65d8d57f838c0c6220395317119c3d33a51868
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2026-08-28 22:18:40 +0200

    change default fan curve

--- common/source/fan.cpp
+++ common/source/fan.cpp
@@ -2,15 +2,15 @@
 #include <algorithm>
 
 const TemperaturePoint defaultTable[TABLE_ENTRIES] = {
-    { .temperature_c = 25,  .fanLevel_f = 0.10f },
-    { .temperature_c = 30,  .fanLevel_f = 0.20f },
-    { .temperature_c = 35,  .fanLevel_f = 0.30f },
-    { .temperature_c = 40,  .fanLevel_f = 0.40f },
-    { .temperature_c = 45,  .fanLevel_f = 0.50f },
-    { .temperature_c = 50,  .fanLevel_f = 0.60f },
-    { .temperature_c = 55,  .fanLevel_f = 0.70f },
-    { .temperature_c = 60,  .fanLevel_f = 0.80f },
-    { .temperature_c = 65,  .fanLevel_f = 0.90f },
+    { .temperature_c = 25,  .fanLevel_f = 0.00f },
+    { .temperature_c = 30,  .fanLevel_f = 0.00f },
+    { .temperature_c = 35,  .fanLevel_f = 0.00f },
+    { .temperature_c = 40,  .fanLevel_f = 0.00f },
+    { .temperature_c = 45,  .fanLevel_f = 0.00f },
+    { .temperature_c = 50,  .fanLevel_f = 0.30f },
+    { .temperature_c = 55,  .fanLevel_f = 0.40f },
+    { .temperature_c = 60,  .fanLevel_f = 0.60f },
+    { .temperature_c = 65,  .fanLevel_f = 0.70f },
     { .temperature_c = 70,  .fanLevel_f = 1.00f },
 };
 
