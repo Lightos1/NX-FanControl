4eb27a5cb8dc8ba4c50b63f5bf6d8ec35f366844
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2026-08-30 17:42:55 +0200

    add back license header, no idea how that got removed

--- common/include/fancontrol/tmp451.hpp
+++ common/include/fancontrol/tmp451.hpp
@@ -1,3 +1,25 @@
+/*
+ * SOC/PCB Temperature driver for Nintendo Switch's TI TMP451
+ *
+ * Copyright (c) 2018 CTCaer
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Modified by: MasaGratoR
+ */
+
 #ifndef __TMP451_H_
 #define __TMP451_H_
 
